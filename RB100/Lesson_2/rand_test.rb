ae437cf3e5d11b1c5ed6202208efa6701c25e6df
kharij_000 <kharijamil@gmail.com>
2022-12-29 00:07:28 -0500

    new rubocop rules edits to rand test(syntax)

--- RB100/Lesson_2/rand_test.rb
+++ RB100/Lesson_2/rand_test.rb
@@ -1,15 +1,14 @@
-merge([1, 2, 3], [4, 5, 6]) # => [1, 4, 2, 5, 3, 6]
+#merge([1, 2, 3], [4, 5, 6]) # => [1, 4, 2, 5, 3, 6]
 
-num = 0
 ar1 = [1, 2, 3, 8]
-ar2 = [4, 5, 6,]
+ar2 = [4, 5, 6]
 ar3 = []
 def merge(a, b, c)
   num = 0
-  until num >= a.length() && num >= b.length()
-    c.push(a[num]) if a[num] != nil 
-    c.push(b[num]) if b[num] != nil 
-    num = num + 1
+  until num >= a.length && num >= b.length
+    c.push(a[num]) if a[num] != nil
+    c.push(b[num]) if b[num] != nil
+    num += 1
   end
 end
 
