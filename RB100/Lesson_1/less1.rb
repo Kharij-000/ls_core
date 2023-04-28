0a13e7def867ee8c81298c40916f53cd68ebb7e8
kharij_000 <kharijamil@gmail.com>
2023-04-28 16:38:34 -0400

    Merge

--- RB100/Lesson_1/less1.rb
+++ RB100/Lesson_1/less1.rb
@@ -3,3 +3,26 @@
     #works with Github Flavored markdown 
   #Good questions vs bad questions
   
+
+  def string_lengths(sentence)
+    strings = sentence.split
+    lengths = []
+    counter = 1
+  
+    until counter == strings.size do
+      word_length = strings[counter - 1].length
+      lengths.push(word_length)
+      counter += 1
+    end
+  
+    lengths
+  end
+
+  p string_lengths("to be or not to be")
+
+
+array = [1, 2, 3]
+  
+mapped_array = array.map { |num| num + 1 }
+
+mapped_array.tap { |value| p value }              # => [2, 3, 4]
